d97af1ecd5661a9b0a66df83a1893bfa1963bb99
efreak <maxjoehnk@gmail.com>
2012-06-20 18:55:59 +0200

    Moved the content of BmInitialize and BmShutdown to the mainclass

--- src/org/efreak1996/Bukkitmanager/BmPlugin.java
+++ src/org/efreak1996/Bukkitmanager/BmPlugin.java
@@ -1,12 +1,23 @@
 package org.efreak1996.Bukkitmanager;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.efreak1996.Bukkitmanager.Commands.BmCommandExecutor;
+import org.efreak1996.Bukkitmanager.Listener.BmBukkitListener;
+import org.efreak1996.Bukkitmanager.Logger.BmLoggingManager;
 import org.efreak1996.Bukkitmanager.PluginManager.BmPluginManager;
+import org.efreak1996.Bukkitmanager.Swing.BmSwing;
+import org.efreak1996.Bukkitmanager.Util.BmIOManager;
+import org.efreak1996.Bukkitmanager.Util.BmThreadManager;
 
 
 /**
- * Our Mainclass for the onLoad, onEnable and onDisable Functions
+ * The mainclass of Bukkitmanager
+ * Initializes everything, shutdowns everything, handles almost everything
  * 
  * @author efreak1996
  * 
@@ -14,18 +25,45 @@ import org.efreak1996.Bukkitmanager.PluginManager.BmPluginManager;
 
 public class BmPlugin extends JavaPlugin {
 	
+	private static Plugin plugin;
+	private static BmConfiguration config;
+	private static BmIOManager io;
+	private static BmLoggingManager logManager;
+	private static BmAutomessageThread msgThread;
+	private static BmAutosaveThread saveThread;
+	private static BmAutobackupThread backupThread;
+	private static BmPermissions permHandler;
+	private static BmThreadManager func;
+	private static BmDatabase db;
+	private static BmSwing swing;
+	private static File rootFolder;
+	private static BmCustomMessageManager msgManager;
+	//private static BmLibraryManager libManager;
+
 	/**
 	 * 
 	 * Runs while shutdown the server
-	 * 
-	 * @see BmShutdown
 	 * 
 	 */
 	
 	@Override
 	public void onDisable() {
-        new BmShutdown();
-	}
+    	db.shutdown();
+    	func.stopThread(BmThreadType.AUTOSAVE);
+    	func.stopThread(BmThreadType.AUTOBACKUP);
+    	func.stopThread(BmThreadType.AUTOMESSAGE);
+		try {
+			FileUtils.cleanDirectory(new File(plugin.getDataFolder().getParentFile().getAbsoluteFile().getParentFile() + File.separator + "backups" + File.separator + "temp"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (config.getDev()) {
+			io.sendConsoleDev("Disabling Development Features!");
+			io.sendConsoleDev("Disabling Swing GUI and Swing Remote Server...");
+			swing.shutdown();
+			io.sendConsoleDev("Development Features disabled!");
+		}
+    	io.sendConsole(io.translate("Plugin.Done"));	}
 	
 	/**
 	 * 
@@ -43,15 +81,89 @@ public class BmPlugin extends JavaPlugin {
 	 * 
 	 * Runs after loading of all plugins and worlds
 	 * 
-	 * @see BmInitialize
-	 * 
 	 */
 	
 	@Override
 	public void onEnable() {
-		new BmInitialize(this);
+		CreateDirs();
+		config = new BmConfiguration();
+		config.initalize();
+		io = new BmIOManager();
+		io.initialize();
+		permHandler = new BmPermissions();
+		permHandler.initialize();
+		func = new BmThreadManager();
+		func.initialize();
+		db = new BmDatabase();
+		db.initialize();
+		logManager = new BmLoggingManager();
+		logManager.initialize();
+		if (config.getDev()) {
+			io.sendConsoleDev("Development Mode enabled!");
+			io.sendConsoleDev("New Features, which aren't complete or unstable will be enabled!");
+			swing = new BmSwing();
+			swing.initialize();
+		}		
+		plugin.getServer().getPluginCommand("bm").setExecutor(new BmCommandExecutor());
+		plugin.getServer().getPluginManager().registerEvents(new BmBukkitListener(), plugin);
+		Threads();
+		if (config.getBoolean("General.Statistics.Enabled")) new BmStats().start();
+		msgManager = new BmCustomMessageManager();
+		msgManager.initialize();
+		new BmFakepluginsManager();
+		io.sendConsole(io.translate("Plugin.Done"));
 	}
 	
+	/**
+	 * 
+	 * Creates the Folderstructure of Bukkitmanager
+	 * 
+	 */
+	
+	private void CreateDirs() {
+		rootFolder = plugin.getDataFolder().getAbsoluteFile().getParentFile().getParentFile();
+		if (new File(plugin.getDataFolder().getAbsoluteFile().getParentFile(), "BukkitManager").exists()) {
+			plugin.getLogger().info("Moving Foldercontent to new Folder...");
+			try {
+				FileUtils.copyDirectory(new File(plugin.getDataFolder().getAbsoluteFile().getParentFile(), "BukkitManager"), plugin.getDataFolder());
+				plugin.getLogger().info("Done! Please delete the folder: " + new File(plugin.getDataFolder().getAbsoluteFile().getParentFile(), "BukkitManager").toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (!(plugin.getDataFolder().exists()))
+				plugin.getDataFolder().mkdirs();
+		File langDir = new File(plugin.getDataFolder() + File.separator + "lang");
+		if (!(langDir.exists()))
+			langDir.mkdirs();
+		if (!(new File(rootFolder, "backups" + File.separator + "temp").exists()))
+			new File(rootFolder, "backups" + File.separator + "temp").mkdirs();
+		try {
+			FileUtils.cleanDirectory(new File(rootFolder, "backups" + File.separator + "temp"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (!(new File(rootFolder, "update").exists()))
+			new File(rootFolder, "update").mkdirs();
+	}
+	
+	/**
+	 * 
+	 * Initialize all Threads
+	 * 
+	 */
+	
+	private void Threads() {
+		msgThread = new BmAutomessageThread();
+		msgThread.initialize();
+		saveThread = new BmAutosaveThread();
+		saveThread.initialize();
+		backupThread = new BmAutobackupThread();
+		backupThread.initialize();
+		if (config.getBoolean("Autosave.Enabled")) func.startThread(BmThreadType.AUTOSAVE);
+		if (config.getBoolean("Automessage.Enabled")) func.startThread(BmThreadType.AUTOMESSAGE);
+		if (config.getBoolean("Autobackup.Enabled")) func.startThread(BmThreadType.AUTOBACKUP);
+	}
 	/**
 	 * 
 	 * Returns the Plugin itself
@@ -75,6 +187,4 @@ public class BmPlugin extends JavaPlugin {
 	public static BmPluginManager getPluginManager() {
 		return new BmPluginManager();
 	}
-	
-	private static Plugin plugin;
 }
