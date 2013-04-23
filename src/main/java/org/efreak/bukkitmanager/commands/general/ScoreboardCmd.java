6256e9c5047e0d1ea5e28a2112e81400be783883
efreak1996 <maxjoehnk@gmail.com>
2013-04-23 14:47:17 +0200

    Removed the length Variable

--- src/main/java/org/efreak/bukkitmanager/commands/general/ScoreboardCmd.java
+++ src/main/java/org/efreak/bukkitmanager/commands/general/ScoreboardCmd.java
@@ -16,15 +16,15 @@ public class ScoreboardCmd extends Command{
 	}
 
 	@Override
-	public boolean execute(CommandSender sender, String[] args, Integer length) {
-		if (args.length < (1 + length)) io.sendFewArgs(sender, "/bm scoreboard (hide|show) [player]");
-		else if (args.length > (2 + length)) io.sendManyArgs(sender, "/bm lang (hide|show) [player]");
+	public boolean execute(CommandSender sender, String[] args) {
+		if (args.length < 1) io.sendFewArgs(sender, "/bm scoreboard (hide|show) [player]");
+		else if (args.length > 2) io.sendManyArgs(sender, "/bm lang (hide|show) [player]");
 		else {
-			if (args.length == (1 + length) && args[0 + length].equalsIgnoreCase("hide")) {
+			if (args.length == 1 && args[0].equalsIgnoreCase("hide")) {
 				ScoreboardManager.hideMainScoreboard(new BmPlayer((OfflinePlayer) sender));
-			}else if (args.length == (1 + length) && args[0 + length].equalsIgnoreCase("show")) {
+			}else if (args.length == 1 && args[0].equalsIgnoreCase("show")) {
 				if (has(sender, "bm.scoreboard.show")) ScoreboardManager.showMainScoreboard(new BmPlayer((OfflinePlayer) sender));
-			}else if (args.length == (2 + length) && args[0 + length].equalsIgnoreCase("hide")) {
+			}else if (args.length == 2 && args[0].equalsIgnoreCase("hide")) {
 			}
 		}
 		return true;
