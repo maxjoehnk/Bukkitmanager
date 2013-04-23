b049a58d1a15fdc95926cc43d3307c218084c44f
efreak1996 <maxjoehnk@gmail.com>
2013-04-23 15:19:12 +0200

    Preparing for more features for the standalone version

---
+++ src/main/java/org/efreak/bukkitmanager/external/Configuration.java
@@ -0,0 +1,50 @@
+package org.efreak.bukkitmanager.external;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+
+	private static Properties properties;
+	private static File propertiesFile;
+	
+	public Configuration() {
+		propertiesFile = new File("bukkitmanager.properties");
+		if (!propertiesFile.exists())
+			try {
+				propertiesFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public Configuration(String fileName) {
+		propertiesFile = new File(fileName);
+		if (!propertiesFile.exists())
+			try {
+				propertiesFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}		
+	}
+	
+	public void load() {
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream(propertiesFile));
+			update();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void update() {
+		properties.setProperty("", "");
+	}
+	
+}
