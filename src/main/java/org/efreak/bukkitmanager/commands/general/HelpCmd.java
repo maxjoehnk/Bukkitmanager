0f3a0e81fd46c175215db504a6a548625714afa8
efreak1996 <maxjoehnk@gmail.com>
2013-04-30 14:33:57 +0200

    Fixed a bug where the help wasn't displayed correct

--- src/main/java/org/efreak/bukkitmanager/commands/general/HelpCmd.java
+++ src/main/java/org/efreak/bukkitmanager/commands/general/HelpCmd.java
@@ -23,11 +23,11 @@ public class HelpCmd extends Command {
 		}
 		int pages = 1;
 		if (topics.size() > 9) pages = (int)(topics.size() / 9F+0.4F);
-		if (args.length == 1) {
-			io.sendHeader(sender, config.getString("IO.HelpHeader").replaceAll("%page%",  String.valueOf(1)).replaceAll("%pages%", String.valueOf(pages)));
+		if (args.length == 0) {
+			io.sendHeader(sender, config.getString("IO.HelpHeader").replaceAll("%page%",  "1").replaceAll("%pages%", String.valueOf(pages)));
 			for (int i = 0; i < 9 && i < topics.size(); i++) io.send(sender, topics.get(i), false);
-		}else if (args.length == 2) {
-			if (args[1].equalsIgnoreCase("caption")) {
+		}else if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("caption")) {
 				io.sendHeader(sender, "BUKKITMANAGER HELP LEGEND");
 	       		io.send(sender, "&c#&f : Number", false);
 	       		io.send(sender, "&cvalue&f : place holder", false);
@@ -37,13 +37,13 @@ public class HelpCmd extends Command {
 	       		io.send(sender, "&c|&f : (or) seperator", false);
 			}else {
 				try {
-					if (new Integer(args[1]) <= pages) {
-						int page = new Integer(args[1]);
-						io.send(sender, config.getString("IO.HelpHeader").replaceAll("%page%",  args[1]).replaceAll("%pages%", String.valueOf(pages)), false);
+					if (new Integer(args[0]) <= pages) {
+						int page = new Integer(args[0]);
+						io.send(sender, config.getString("IO.HelpHeader").replaceAll("%page%",  args[0]).replaceAll("%pages%", String.valueOf(pages)), false);
 						for (int i = 9*page-9; i < 9*page && i < topics.size()-1; i++) io.send(sender, topics.get(i), false);
 					}else io.sendError(sender, "This Page doesn't exist.");
 				}catch (NumberFormatException e) {
-					if (CommandCategory.valueOf(args[1].toUpperCase()) != null) {
+					if (CommandCategory.valueOf(args[0].toUpperCase()) != null) {
 						io.send(sender, "Command..");
 					}
 				}
