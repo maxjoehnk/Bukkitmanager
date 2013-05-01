3a283de281170a4d858e3d58acd7d818b64933a3
e-freak <maxjoehnk@gmail.com>
2013-05-01 15:07:09 +0200

    Modified Alias registration to use reflection, removes the need for a new build for every new CraftBukkit Version

--- src/main/java/org/efreak/bukkitmanager/commands/Alias.java
+++ src/main/java/org/efreak/bukkitmanager/commands/Alias.java
@@ -1,10 +1,15 @@
 package org.efreak.bukkitmanager.commands;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Scanner;
 
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginIdentifiableCommand;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.SimplePluginManager;
 
 import org.efreak.bukkitmanager.Bukkitmanager;
 import org.efreak.bukkitmanager.Configuration;
@@ -31,7 +36,12 @@ public class Alias extends org.bukkit.command.Command implements PluginIdentifia
 		description = desc;
 		usageMessage = "Use /bm help for more Help";
 		try {
-			if (config.getAlias(name)) ((org.bukkit.craftbukkit.v1_5_R3.CraftServer) plugin.getServer()).getCommandMap().register(name, this);
+			if (config.getAlias(name)) {
+				Field field = SimplePluginManager.class.getDeclaredField("commandMap");
+				field.setAccessible(true);
+				SimpleCommandMap scm = (SimpleCommandMap) field.get(plugin.getServer().getPluginManager());
+				scm.register(name, this);
+			}
 		}catch (Exception e) {
 			io.sendConsoleError("Error registering Commandalias '" + name + "'. Try updating Bukkitmanager!");
 			if (config.getDebug()) e.printStackTrace();
