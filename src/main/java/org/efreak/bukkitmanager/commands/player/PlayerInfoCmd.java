243d62b194656569958bc78f542ac30aabcf2a79
e-freak <maxjoehnk@gmail.com>
2013-05-01 17:07:34 +0200

    Fixed some bugs

--- src/main/java/org/efreak/bukkitmanager/commands/player/PlayerInfoCmd.java
+++ src/main/java/org/efreak/bukkitmanager/commands/player/PlayerInfoCmd.java
@@ -20,7 +20,7 @@ public class PlayerInfoCmd extends Command {
 	@Override
 	public boolean execute(CommandSender sender, String[] args) {
 		if (args.length < 1) io.sendFewArgs(sender, "/bm player info [player]");
-		else if (args.length > 2) io.sendFewArgs(sender, "/bm player info [player]");
+		else if (args.length > 2) io.sendManyArgs(sender, "/bm player info [player]");
 		else {
 			if (args.length == 1 && sender instanceof Player) {
 				if (has(sender, "bm.player.info.own")) {
@@ -32,13 +32,13 @@ public class PlayerInfoCmd extends Command {
 					io.send(sender, "Level:      " + player.getLevel(), false);
 					io.send(sender, "World:      " + player.getWorld().getName(), false);
 					io.send(sender, "Position:   {x=" + player.getX() + "|y=" + player.getY() + "|z=" + player.getZ() + "}", false);
-				}else io.sendError(sender, "You don't have permission to do that");
+				}
 			}else if (args.length == 2) {
 				if (has(sender, "bm.player.info.other")) {
 					OfflinePlayer offPlayer = Bukkit.getOfflinePlayer(args[1]);
 					if (offPlayer != null) {
 						BmPlayer player = new BmPlayer(offPlayer);
-						io.sendHeader(sender, "PLAYER: " + sender.getName().toUpperCase());
+						io.sendHeader(sender, "PLAYER: " + player.getName().toUpperCase());
 						io.send(sender, "Health:    " + player.getHealth() + "/" + player.getMaxHealth(), false);
 						io.send(sender, "Foodlevel: " + player.getFoodLevel() + "/20", false);
 						io.send(sender, "Gamemode:  " + player.getGameMode().toString(), false);
@@ -46,8 +46,8 @@ public class PlayerInfoCmd extends Command {
 						io.send(sender, "World:     " + player.getWorld().getName(), false);
 						io.send(sender, "Position:  {x=" + player.getX() + "|y=" + player.getY() + "|z=" + player.getZ() + "}", false);
 					}else io.sendError(sender, io.translate("Command.Player.UnknownPlayer"));
-				}else io.sendError(sender, "You don't have permission to do that");
-			}
+				}
+			}else io.sendError(sender, io.translate("Command.Player.SpecifyPlayer"));
 
 		}
 		return true;
