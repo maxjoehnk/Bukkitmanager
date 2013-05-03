f8f2889174f306f8ab944a0bbc1381f4a650d28f
e-freak <maxjoehnk@gmail.com>
2013-05-03 20:48:39 +0200

    Implemented lastseen time formatting

--- src/main/java/org/efreak/bukkitmanager/commands/player/PlayerLastseenCmd.java
+++ src/main/java/org/efreak/bukkitmanager/commands/player/PlayerLastseenCmd.java
@@ -1,6 +1,8 @@
 package org.efreak.bukkitmanager.commands.player;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -21,12 +23,22 @@ public class PlayerLastseenCmd extends Command {
 		else if (args.length > 2) io.sendManyArgs(sender, "/bm player lastseen [player]");
 		else {
 			if (args.length == 1 && sender instanceof Player) {
-				if (has(sender, "bm.player.lastseen.your")) io.send(sender, io.translate("Command.Player.Lastseen.Your").replaceAll("%lastseen%", String.valueOf(((Player) sender).getLastPlayed())));
+				if (has(sender, "bm.player.lastseen.your")) {
+					Calendar calendar = new GregorianCalendar();
+					calendar.setTimeInMillis(((OfflinePlayer) sender).getLastPlayed());
+					String date = calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR);
+					String time = calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE);
+					io.send(sender, io.translate("Command.Player.Lastseen.Your").replaceAll("%lastseen_date%", date).replaceAll("%lastseen_time%", time));
+				}
 			}
 			else if (args.length == 2) {
 				if (has(sender, "bm.player.lastseen.other")) {
 					OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
-					if (player != null) io.send(sender, io.translate("Command.Player.Lastseen.Other").replaceAll("%player%", player.getName()).replaceAll("%lastseen%", String.valueOf(player.getLastPlayed())));
+					Calendar calendar = new GregorianCalendar();
+					calendar.setTimeInMillis(player.getLastPlayed());
+					String date = calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR);
+					String time = calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE);
+					if (player != null) io.send(sender, io.translate("Command.Player.Lastseen.Other").replaceAll("%player%", player.getName()).replaceAll("%lastseen_date%", date).replaceAll("%lastseen_time%", time));
 					else io.sendError(sender, io.translate("Command.Player.UnknownPlayer"));
 				}
 			}else io.sendError(sender, io.translate("Command.Player.SpecifyPlayer"));
