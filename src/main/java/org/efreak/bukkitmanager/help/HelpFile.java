60b538ffcccc823bc4e1ed5ed6cea11858faaa65
e-freak <maxjoehnk@gmail.com>
2013-05-03 20:08:59 +0200

    Updated Helpentries

--- src/main/java/org/efreak/bukkitmanager/help/HelpFile.java
+++ src/main/java/org/efreak/bukkitmanager/help/HelpFile.java
@@ -41,26 +41,26 @@ public class HelpFile {
 		update("Password", "Gets/Sets the Remote Password");
 		
 		update("Autobackup.Backup", "Performs a Backup");
+		update("Autobackup.Interval", "Gets/Sets the Autobackup Interval");
+		update("Autobackup.Restart", "Restarts the Autobackupthread");
 		update("Autobackup.Start", "Starts the Autobackupthread");
 		update("Autobackup.Stop", "Stops the Autobackupthread");
-		update("Autobackup.Restart", "Restarts the Autobackupthread");
-		update("Autobackup.Interval", "Gets/Sets the Autobackup Interval");
 		
 		update("Automessage.Add", "Lists all Messages");
-		update("Automessage.Remove", "Removes a Message");
 		update("Automessage.Get", "Gets a Message");
+		update("Automessage.Interval", "Gets/Sets the Automessage Interval");	
 		update("Automessage.List", "Lists all Messages");
+		update("Automessage.Remove", "Removes a Message");
+		update("Automessage.Restart", "Restarts the Automessagethread");
 		update("Automessage.Send", "Sends a Message");
 		update("Automessage.Start", "Starts the Automessagethread");
 		update("Automessage.Stop", "Stops the Automessagethread");
-		update("Automessage.Restart", "Restarts the Automessagethread");
-		update("Automessage.Interval", "Gets/Sets the Automessage Interval");	
 		
+		update("Autosave.Interval", "Gets/Sets the Autosave Interval");
+		update("Autosave.Restart", "Restarts the Autosavethread");
 		update("Autosave.Save", "Performs a Save");
 		update("Autosave.Start", "Starts the Autosavethread");
 		update("Autosave.Stop", "Stops the Autosavethread");
-		update("Autosave.Restart", "Restarts the Autosavethread");
-		update("Autosave.Interval", "Gets/Sets the Autosave Interval");
 		
 		update("Bukkit.Config", "Modify the server.properties");
 		update("Bukkit.Info", "Shows info about Bukkit");
@@ -68,36 +68,46 @@ public class HelpFile {
 				
 		update("Player.Chat", "Send a Message with another Player");
 		update("Player.Cmd", "Perform a Command with another Player");
-		update("Player.Displayname.Get", "Get the Players Displayname");
-		update("Player.Displayname.Set", "Set the Players Displayname");
-		update("Player.Displayname.Reset", "Reset the Players Displayname");
+		update("Player.Displayname", "Get/Set/Reset the Players Displayname");
 		update("Player.Exp", "Modify the Experience of a Player");
 		update("Player.Firstseen", "Gets the first Online Time of the Player");
 		update("Player.Food", "Modify the Foodlevel of a Players");
 		update("Player.Gamemode", "Changes the Gamemode");
 		update("Player.Health", "Modify the Health of a Players");
 		update("Player.Hide", "Hides a Player");
+		update("Player.Info", "Displays Informations about a Player");
 		update("Player.Lastseen", "Gets the last Online Time of the Player");
 		update("Player.Level", "Get/Set the Level of a Player");
 		update("Player.Listname", "Modify the Players Listname");
 		update("Player.Load", "Loads the Player.dat");
+		update("Player.Location", "Get the exact Player Location");
 		update("Player.Save", "Saves the Player.dat");
 		update("Player.Show", "Shows a Player");
 		update("Player.Time", "Modify the Players Time");
+		update("Player.Tp", "Teleport a Player to another Player");
 
 		update("Plugin.Config", "Modify Plugins Configs");
+		update("Plugin.Delete", "Delete Plugins");
 		update("Plugin.Disable", "Disable Plugins");
 		update("Plugin.Enable", "Enable Plugins");
 		update("Plugin.Info", "Shows info about a Plugin");
-		update("Plugin.Install", "Installs a new Plugin");
-		update("Plugin.List", "Lists all Plugins");
-		update("Plugin.Load", "Loads Plugins without a reload");
-		update("Plugin.Restart", "Restarts a Plugin");
-		update("Plugin.Update", "Updates a Plugin");
+		update("Plugin.Install", "Install a new Plugin");
+		update("Plugin.List", "List all Plugins");
+		update("Plugin.Load", "Load Plugins without a reload");
+		update("Plugin.Reload", "Reload Plugins");
+		update("Plugin.Restart", "Restart Plugins");
+		update("Plugin.Unload", "Unload Plugins");
+		update("Plugin.Update", "Update Plugins");
 		
 		update("Server.Info", "Gives you general Server Information");
-		update("Server.Network", "Gives you informations about the Network Adapter");
 		update("Server.Load", "Displays the Serverload");		
+		update("Server.Network", "Gives you informations about the Network Adapter");
+		
+		update("World.Create", "Create and Load a new World");
+		update("World.Delte", "Delete a world");
+		update("World.List", "List all loaded Worlds");
+		update("World.Load", "Load an already created World");
+		update("World.Unload", "Unload a world");
 	}
 	
 	public String getHelp(String key) {
