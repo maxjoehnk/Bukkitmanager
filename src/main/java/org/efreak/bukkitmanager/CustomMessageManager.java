033f1729de5238d44bc7d239b770c82cb8c51894
e-freak <maxjoehnk@gmail.com>
2013-07-11 16:36:04 +0200

    Added UnknownCommand Custom Messages

--- src/main/java/org/efreak/bukkitmanager/CustomMessageManager.java
+++ src/main/java/org/efreak/bukkitmanager/CustomMessageManager.java
@@ -13,15 +13,22 @@ public class CustomMessageManager {
 	private static Configuration config;
 	private static CustomMessageListener listener;
 	private static IOManager io;
+	
 	private static List<String> playerJoin;
 	private static int playerJoinIndex;
 	private static List<Integer> playerJoinDisplayed;
+	
 	private static List<String> playerLeave;
 	private static int playerLeaveIndex;
 	private static List<Integer> playerLeaveDisplayed;
+	
 	private static List<String> playerKick;
 	private static int playerKickIndex;
 	private static List<Integer> playerKickDisplayed;
+
+	private static List<String> unknownCommand;
+	private static int unknownCommandIndex;
+	private static List<Integer> unknownCommandDisplayed;
 
 	static {
 		config = Bukkitmanager.getConfiguration();
@@ -33,16 +40,24 @@ public class CustomMessageManager {
 		if (config.getBoolean("CustomMessages.Enabled")) {
 			io.sendConsole(io.translate("CustomMessages.Loading"));
 			Bukkit.getServer().getPluginManager().registerEvents(listener, Bukkitmanager.getInstance());
+			
 			playerJoin = config.getStringList("CustomMessages.Join");
 			playerJoinIndex = new Random().nextInt(playerJoin.size());
 			playerJoinDisplayed = new ArrayList<Integer>();
+			
 			playerLeave = config.getStringList("CustomMessages.Leave");
 			playerLeaveIndex = new Random().nextInt(playerLeave.size());
 			playerLeaveDisplayed = new ArrayList<Integer>();
+			
 			playerKick = config.getStringList("CustomMessages.Kick");
 			playerKickIndex = new Random().nextInt(playerKick.size());
 			playerKickDisplayed = new ArrayList<Integer>();
-			io.sendConsole(io.translate("CustomMessages.Loaded").replaceAll("%msgCount%", String.valueOf(playerJoin.size() + playerLeave.size() + playerKick.size())));
+
+			unknownCommand = config.getStringList("CustomMessages.UnknownCommand");
+			unknownCommandIndex = new Random().nextInt(playerKick.size());
+			unknownCommandDisplayed = new ArrayList<Integer>();
+
+			io.sendConsole(io.translate("CustomMessages.Loaded").replaceAll("%msgCount%", String.valueOf(playerJoin.size() + playerLeave.size() + playerKick.size() + unknownCommand.size())));
 		}
 	}
 	
@@ -72,6 +87,16 @@ public class CustomMessageManager {
 		while (playerKickDisplayed.contains(playerKickIndex)) {
 			if (playerKickDisplayed.size() == playerKick.size()) playerKickDisplayed.clear();
 			playerKickIndex = new Random().nextInt(playerKick.size());
+		}
+		return text;
+	}
+	
+	public String getUnknownCommand() {
+		String text = unknownCommand.get(unknownCommandIndex);
+		unknownCommandDisplayed.add(unknownCommandIndex);
+		while (unknownCommandDisplayed.contains(unknownCommandIndex)) {
+			if (unknownCommandDisplayed.size() == unknownCommand.size()) unknownCommandDisplayed.clear();
+			unknownCommandIndex = new Random().nextInt(unknownCommand.size());
 		}
 		return text;
 	}
